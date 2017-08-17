054c7375a049016fbcc0f8111168eaedf7e6a3a3
Adam Trask <adam.p.trask@gmail.com>
2017-08-17 10:23:46 -0400

    Fix rules management console commands

--- rules/management/commands/UploadDirectory.py
+++ rules/management/commands/UploadDirectory.py
@@ -1,9 +1,10 @@
 import os
+import logging
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import get_user_model
-from django.conf import settings
+from django.contrib.auth.models import Group
 
 from rules.models import YaraRule
 from core.services import parse_rule_submission
@@ -16,6 +17,7 @@ except ImportError:
     from scandir import scandir, walk
 
 User = get_user_model()
+logging.basicConfig(level=logging.INFO)
 
 
 class Command(BaseCommand):
@@ -30,25 +32,35 @@ class Command(BaseCommand):
         # Additional required arguments
         parser.add_argument('--user', required=True)
 
+        parser.add_argument('--group', required=True)
+
+        # Optional arguments
         parser.add_argument('--source',
-                            required=True,
-                            choices=[choice for choice in settings.YARA_SOURCE])
+                            default='')
 
         parser.add_argument('--category',
-                            required=True,
-                            choices=[choice for choice in settings.YARA_CATEGORY])
+                            default='')
 
     def handle(self, *args, **options):
 
         username = options['user']
+        groupname = options['group']
         rules_source = options['source']
         rules_category = options['category']
 
         try:
             user = User.objects.get(username=username)
         except ObjectDoesNotExist:
-            self.stdout.write('[!] Specified user does not exist')
-        else:
+            user = None
+            logging.error('[!] Specified user does not exist')
+
+        try:
+            group = Group.objects.get(name=groupname)
+        except ObjectDoesNotExist:
+            group = None
+            logging.error('[!] Specified group does not exist')
+
+        if user and group:
             for yara_directory in options['rule_directories']:
                 # Scan an entire directory recursively
                 if os.path.isdir(yara_directory):
@@ -67,16 +79,22 @@ class Command(BaseCommand):
 
                                 # Identify any parsing errors that occur
                                 if parsing_error:
-                                	message = '[!] Parsing Error: {}'.format(parsing_error)
+                                    message = '[!] Parsing Error: {}'.format(parsing_error)
+                                    logging.error(message)
                                 else:
                                     # Save successfully parsed rules
                                     save_results = YaraRule.objects.process_parsed_rules(parsed_rules,
                                                                                          rules_source,
                                                                                          rules_category,
-                                                                                         submitter=user,
+                                                                                         user, group,
                                                                                          status=YaraRule.ACTIVE_STATUS)
                                     upload_count = save_results['rule_upload_count']
                                     collision_count = save_results['rule_collision_count']
                                     message = '[✓] Successfully uploaded {} rules and prevented {} rule collisions from {}'.format(upload_count, collision_count, file_path)
+                                    logging.info(message)
 
-                                self.stdout.write(message)
+                                    for error in save_results['errors']:
+                                        logging.error(error)
+
+                                    for warning in save_results['warnings']:
+                                        logging.warn(warning)
--- rules/management/commands/UploadMasterFile.py
+++ rules/management/commands/UploadMasterFile.py
@@ -1,15 +1,18 @@
 import os
 import re
+import logging
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import get_user_model
-from django.conf import settings
 
 from rules.models import YaraRule
 from core.services import parse_rule_submission
 
 User = get_user_model()
+
+# Configure Logging
+logging.basicConfig(level=logging.INFO)
 
 
 class Command(BaseCommand):
@@ -22,26 +25,35 @@ class Command(BaseCommand):
 
         # Additional required arguments
         parser.add_argument('--user', required=True)
+        parser.add_argument('--group', required=True)
 
+        # Optional arguments
         parser.add_argument('--source',
-                            required=True,
-                            choices=[choice for choice in settings.YARA_SOURCE])
+                            default='')
 
         parser.add_argument('--category',
-                            required=True,
-                            choices=[choice for choice in settings.YARA_CATEGORY])
+                            default='')
 
     def handle(self, *args, **options):
 
         username = options['user']
+        groupname = options['group']
         rules_source = options['source']
         rules_category = options['category']
 
         try:
             user = User.objects.get(username=username)
         except ObjectDoesNotExist:
-            self.stdout.write('[!] Specified user does not exist')
-        else:
+            user = None
+            logging.error('[!] Specified user does not exist')
+
+        try:
+            group = Group.objects.get(name=groupname)
+        except ObjectDoesNotExist:
+            group = None
+            logging.error('[!] Specified group does not exist')
+
+        if user and group:
             for yara_master in options['rule_masters']:
 
                 if os.path.isfile(yara_master):
@@ -66,15 +78,21 @@ class Command(BaseCommand):
                         # Identify any parsing errors that occur
                         if parsing_error:
                             message = '[!] Parsing Error: {}'.format(parsing_error)
+                            logging.error(message)
                         else:
                             # Save successfully parsed rules
                             save_results = YaraRule.objects.process_parsed_rules(parsed_rules,
                                                                                  rules_source,
                                                                                  rules_category,
-                                                                                 submitter=user,
+                                                                                 user, group,
                                                                                  status=YaraRule.ACTIVE_STATUS)
                             upload_count = save_results['rule_upload_count']
                             collision_count = save_results['rule_collision_count']
                             message = '[✓] Successfully uploaded {} rules and prevented {} rule collisions from {}'.format(upload_count, collision_count, file_path)
+                            logging.info(message)
 
-                        self.stdout.write(message)
+                            for error in save_results['errors']:
+                                logging.error(error)
+
+                            for warning in save_results['warnings']:
+                                logging.warn(warning)
