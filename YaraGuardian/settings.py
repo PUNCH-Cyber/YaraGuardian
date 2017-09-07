85aba02dc1a241644201d2570444a7541d76949c
Marcus LaFerrera <marcus@randomhack.org>
2017-09-07 14:32:52 -0400

    Removed whitenoise load by default

--- YaraGuardian/settings.py
+++ YaraGuardian/settings.py
@@ -163,7 +163,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 TEMPLATES = [
