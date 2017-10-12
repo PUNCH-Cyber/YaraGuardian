d36e770dd94fbee08ad92dbb4fa11be5a15f46ad
Adam Trask <taskr@Hypnos.fios-router.home>
2017-10-12 13:19:34 -0400

    Update URLs

--- YaraGuardian/API/groups.py
+++ YaraGuardian/API/groups.py
@@ -6,9 +6,9 @@ from core.REST_views import (GroupsView, GroupDetailsView,
 
 urlpatterns = [
     url(r'^$', GroupsView.as_view(), name='groups'),
-    url(r'^(?P<group_name>\w+)/$', GroupDetailsView.as_view(), name='group-details'),
-    url(r'^(?P<group_name>\w+)/members/$', GroupMembersView.as_view(), name='group-members'),
-    url(r'^(?P<group_name>\w+)/admins/$', GroupAdminsView.as_view(), name='group-admins'),
-    url(r'^(?P<group_name>\w+)/sources/$', GroupSourcesView.as_view(), name='group-sources'),
-    url(r'^(?P<group_name>\w+)/categories/$', GroupCategoriesView.as_view(), name='group-categories')
+    url(r'^(?P<group_name>[\s\w]+)/$', GroupDetailsView.as_view(), name='group-details'),
+    url(r'^(?P<group_name>[\s\w]+)/members/$', GroupMembersView.as_view(), name='group-members'),
+    url(r'^(?P<group_name>[\s\w]+)/admins/$', GroupAdminsView.as_view(), name='group-admins'),
+    url(r'^(?P<group_name>[\s\w]+)/sources/$', GroupSourcesView.as_view(), name='group-sources'),
+    url(r'^(?P<group_name>[\s\w]+)/categories/$', GroupCategoriesView.as_view(), name='group-categories')
 ]
--- YaraGuardian/API/rules.py
+++ YaraGuardian/API/rules.py
@@ -18,19 +18,19 @@ urlpatterns = [
 
     url(r'^$', RulesetsListingView.as_view(), name='rulesets'),
 
-    url(r'^(?P<group_name>\w+)/$', RulesetView.as_view(), name='ruleset'),
-    url(r'^(?P<group_name>\w+)/stats/$', RulesetStatsView.as_view(), name='ruleset-stats'),
-    url(r'^(?P<group_name>\w+)/search/$', RulesetSearchView.as_view(), name='ruleset-search'),
-    url(r'^(?P<group_name>\w+)/export/$', RulesetExportView.as_view(), name='ruleset-export'),
-    url(r'^(?P<group_name>\w+)/deconflict/$', RulesetDeconflictView.as_view(), name='ruleset-deconflict'),
-    url(r'^(?P<group_name>\w+)/bulk/$', RulesetBulkEditView.as_view(), name='ruleset-bulk'),
+    url(r'^(?P<group_name>[\s\w]+)/$', RulesetView.as_view(), name='ruleset'),
+    url(r'^(?P<group_name>[\s\w]+)/stats/$', RulesetStatsView.as_view(), name='ruleset-stats'),
+    url(r'^(?P<group_name>[\s\w]+)/search/$', RulesetSearchView.as_view(), name='ruleset-search'),
+    url(r'^(?P<group_name>[\s\w]+)/export/$', RulesetExportView.as_view(), name='ruleset-export'),
+    url(r'^(?P<group_name>[\s\w]+)/deconflict/$', RulesetDeconflictView.as_view(), name='ruleset-deconflict'),
+    url(r'^(?P<group_name>[\s\w]+)/bulk/$', RulesetBulkEditView.as_view(), name='ruleset-bulk'),
 
-    url(r'^(?P<group_name>\w+)/(?P<rule_pk>\d+)/$', RuleDetailsView.as_view(), name='rule-details'),
-    url(r'^(?P<group_name>\w+)/(?P<rule_pk>\d+)/tags/(?P<tag>\w+)/$', RuleTagsView.as_view(), name='rule-tags'),
-    url(r'^(?P<group_name>\w+)/(?P<rule_pk>\d+)/metadata/(?P<metakey>\w+)/$', RuleMetadataView.as_view(), name='rule-metadata'),
-    url(r'^(?P<group_name>\w+)/(?P<rule_pk>\d+)/comments/$', RuleCommentsView.as_view(), name='rule-comments'),
+    url(r'^(?P<group_name>[\s\w]+)/(?P<rule_pk>\d+)/$', RuleDetailsView.as_view(), name='rule-details'),
+    url(r'^(?P<group_name>[\s\w]+)/(?P<rule_pk>\d+)/tags/(?P<tag>\w+)/$', RuleTagsView.as_view(), name='rule-tags'),
+    url(r'^(?P<group_name>[\s\w]+)/(?P<rule_pk>\d+)/metadata/(?P<metakey>\w+)/$', RuleMetadataView.as_view(), name='rule-metadata'),
+    url(r'^(?P<group_name>[\s\w]+)/(?P<rule_pk>\d+)/comments/$', RuleCommentsView.as_view(), name='rule-comments'),
 
-    url(r'^(?P<group_name>\w+)/(?P<rule_pk>\d+)/comments/(?P<comment_pk>\d+)/$',
+    url(r'^(?P<group_name>[\s\w]+)/(?P<rule_pk>\d+)/comments/(?P<comment_pk>\d+)/$',
         RuleCommentDetailsView.as_view(),
         name='rule-comment-details'),
 ]
