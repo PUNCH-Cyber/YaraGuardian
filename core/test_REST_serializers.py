8fc279e37369008225b03233fafbe76ff42ce704
Adam Trask <taskr@Erebus.local>
2017-11-30 21:51:38 -0500

    Testing updates

--- core/test_REST_serializers.py
+++ core/test_REST_serializers.py
@@ -8,47 +8,108 @@ from .REST_serializers import (PublicGroupSerializer,
                                GroupMetaUpdateSerializer,
                                PrivateUserSerializer)
 
+from rules.models import YaraRule
+
 
 class PublicGroupSerializerTestCase(TestCase):
-    # TO-DO
 
     @classmethod
     def setUpTestData(cls):
         cls.user = generate_test_user()
         cls.group = cls.user.groups.get()
+        cls.serializer = PublicGroupSerializer(instance=cls.group)
+
+    def test_expected_fields(self):
+        data = self.serializer.data
+        self.assertCountEqual(data.keys(), ['name',
+                                            'owner',
+                                            'rule_count',
+                                            'member_count'])
 
 
 class PrivateGroupSerializerTestCase(TestCase):
-    # TO-DO
     
     @classmethod
     def setUpTestData(cls):
         cls.user = generate_test_user()
         cls.group = cls.user.groups.get()
+        cls.serializer = PrivateGroupSerializer(instance=cls.group)
+
+    def test_expected_fields(self):
+        data = self.serializer.data
+        self.assertCountEqual(data.keys(), ['name',
+                                            'owner',
+                                            'members',
+                                            'source_required',
+                                            'source_options',
+                                            'category_required',
+                                            'category_options',
+                                            'nonprivileged_submission_status',
+                                            'rule_count'])
 
 
 class GroupMetaUpdateSerializerTestCase(TestCase):
-    # TO-DO
-    
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = generate_test_user()
-        cls.group = cls.user.groups.get()
+
+    def setUp(self):
+        self.user = generate_test_user()
+        self.group = self.user.groups.get()
+
+    def test_valid_update_source_required(self):
+        self.assertTrue(self.group.groupmeta.source_required)
+        serializer = GroupMetaUpdateSerializer(self.group.groupmeta,
+                                               data={'source_required': False},
+                                               partial=True)
+        if serializer.is_valid():
+            serializer.save()
+
+        self.assertFalse(self.group.groupmeta.source_required)
+
+    def test_valid_update_category_required(self):
+        self.assertTrue(self.group.groupmeta.category_required)
+        serializer = GroupMetaUpdateSerializer(self.group.groupmeta,
+                                               data={'category_required': False},
+                                               partial=True)
+        if serializer.is_valid():
+            serializer.save()
+
+        self.assertFalse(self.group.groupmeta.category_required)
+
+    def test_valid_update_nonprivileged_submission_status(self):
+        self.assertEqual(self.group.groupmeta.nonprivileged_submission_status, YaraRule.PENDING_STATUS)
+        serializer = GroupMetaUpdateSerializer(self.group.groupmeta,
+                                               data={'nonprivileged_submission_status': YaraRule.ACTIVE_STATUS},
+                                               partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+
+        self.assertEqual(self.group.groupmeta.nonprivileged_submission_status, YaraRule.ACTIVE_STATUS)
 
 
 class PublicUserSerializerTestCase(TestCase):
-    # TO-DO
     
     @classmethod
     def setUpTestData(cls):
         cls.user = generate_test_user()
         cls.group = cls.user.groups.get()
+        cls.serializer = PublicUserSerializer(instance=cls.user)
+
+    def test_expected_fields(self):
+        data = self.serializer.data
+        self.assertCountEqual(data.keys(), ['username'])
 
 
 class PrivateUserSerializerTestCase(TestCase):
-    # TO-DO
     
     @classmethod
     def setUpTestData(cls):
         cls.user = generate_test_user()
         cls.group = cls.user.groups.get()
+        cls.serializer = PrivateUserSerializer(instance=cls.user)
+
+    def test_expected_fields(self):
+        data = self.serializer.data
+        self.assertCountEqual(data.keys(), ['email',
+                                            'username',
+                                            'groups',
+                                            'api_token'])
--- core/test_REST_views.py
+++ core/test_REST_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import Group
 from django.core.urlresolvers import reverse
 from rest_framework.test import APIRequestFactory, force_authenticate
 
@@ -13,9 +14,12 @@ from .REST_views import (AccountView,
                          GroupSourcesView,
                          GroupCategoriesView)
 
+from .REST_serializers import (PrivateGroupSerializer,
+                               PublicGroupSerializer,
+                               PrivateUserSerializer)
+
 
 class AccountViewTestCase(TestCase):
-    # TO-DO
 
     def setUp(self):
         self.url = reverse('account')
@@ -24,26 +28,83 @@ class AccountViewTestCase(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.user = generate_test_user()
+        cls.user = generate_test_user(username='VIEW_TESTER_0001')
         cls.group = cls.user.groups.get()
+
+    def test_authenticated_get_request(self):
+        request = self.factory.get(self.url)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, PrivateUserSerializer(self.user).data)
+
+    def test_unauthenticated_get_request(self):
+        request = self.factory.get(self.url)
+        response = self.view(request)
+        self.assertEqual(response.status_code, 403)
 
 
 class AccountGroupsViewTestCase(TestCase):
-    # TO-DO
 
     def setUp(self):
         self.url = reverse('account-groups')
         self.factory = APIRequestFactory()
         self.view = AccountGroupsView.as_view()
+        self.user = generate_test_user(username='VIEW_TESTER_0001')
+        self.group = self.user.groups.get()
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = generate_test_user()
-        cls.group = cls.user.groups.get()
+    def test_authenticated_get_request(self):
+        request = self.factory.get(self.url)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, PrivateGroupSerializer(self.user.groups.all(), many=True).data)
+
+    def test_unauthenticated_get_request(self):
+        request = self.factory.get(self.url)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_valid_authenticated_post_request(self):
+        request = self.factory.post(self.url, {'name': 'NEW_GROUP'})
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(self.user.groups.filter(name='NEW_GROUP').exists())
+
+    def test_invalid_authenticated_post_request_blank_name(self):
+        request = self.factory.post(self.url, {'name': ''})
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_authenticated_post_request_already_exists(self):
+        request = self.factory.post(self.url, {'name': 'VIEW_TESTER_0001'})
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_authenticated_post_request_spaces_in_name(self):
+        request = self.factory.post(self.url, {'name': 'NEW GROUP'})
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_unauthenticated_post_request(self):
+        request = self.factory.post(self.url, {'name': 'NEW_GROUP'})
+        response = self.view(request)
+        
+        self.assertEqual(response.status_code, 403)
 
 
 class GroupsViewTestCase(TestCase):
-    # TO-DO
 
     def setUp(self):
         self.url = reverse('groups')
@@ -54,6 +115,19 @@ class GroupsViewTestCase(TestCase):
     def setUpTestData(cls):
         cls.user = generate_test_user()
         cls.group = cls.user.groups.get()
+
+    def test_authenticated_get_request(self):
+        request = self.factory.get(self.url)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, PublicGroupSerializer(Group.objects.all(), many=True).data)
+
+    def test_unauthenticated_get_request(self):
+        request = self.factory.get(self.url)
+        response = self.view(request)
+        self.assertEqual(response.status_code, 403)
 
 
 class GroupDetailsViewTestCase(TestCase):
