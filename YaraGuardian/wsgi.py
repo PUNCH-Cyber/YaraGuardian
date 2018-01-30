7849e604e22d33a9126c57740133b29b619076da
Adam Trask <taskr@Hypnos.local>
2018-01-29 22:27:22 -0500

    Update wsgi

--- YaraGuardian/wsgi.py
+++ YaraGuardian/wsgi.py
@@ -1,5 +1,12 @@
 import os
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "YaraGuardian.settings")
-application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+from django.conf import settings
+
+if settings.SERVE_STATIC:
+    application = DjangoWhiteNoise(get_wsgi_application())
+else:
+    application = get_wsgi_application()
